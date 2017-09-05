e6d76b8069abd515fbd5b0c377e5a9e68eb7668a
rhostem <syoung.j@gmail.com>
2017-09-06 02:39:03 +0900

    custom axios 인스턴스 추가. dotenv 파일을 통해 baseUrl 설정

--- src/api/example.js
+++
@@ -1,5 +0,0 @@
-export const fetchGithubRepo = search => {
-  return fetch(
-    `https://api.github.com/search/repositories?q=${search}&sort=stars&order=desc&limit=10`,
-  ).then(res => res.json());
-};
